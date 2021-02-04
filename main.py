62db11a76b771b9c40f1a4e29de1cebd16442429
IsaacYantes23 <817296@mystma.org>
2021-02-04 18:18:36 +0000

    letterpsoition try 1

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-#WRITE YOUR CODE IN THIS FILE
+def letterPos(w,p):
+    letter = len(w)
+    for i in range(0,len(w)):
+        if letter + i:
+           
+            return w[i]
+        
+    else:
+            return 0
+
+print(letterPos("rat","t"))
